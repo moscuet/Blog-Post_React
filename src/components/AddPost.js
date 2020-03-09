18b23617e60ebcdaa5d85e61860deb8fca88cafd
Michele Zucca <michele.zucca81@gmail.com>
2020-03-09 17:11:10 +0200

    added router boilerplate

---
+++ src/components/AddPost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddPost = () => {
+    return (
+        <div>
+            <h1>This is the Add Post</h1>
+        </div>
+    )
+}
+
+export default AddPost;
---
+++ src/components/EditPost.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const EditPost = () => {
+    return (
+        <div>
+            <h1>This is the Edit Post</h1>
+        </div>
+    )
+}
+
+export default EditPost;
